19f208d32d989435ca5a32b2ebe5021077b54942
wllysses <wllyssestavares@gmail.com>
2023-11-13 10:54:14 -0300

    style: header fixed style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Vacancys } from "@/components/ui/vacancys";
 
 export default function Home() {
   return (
-    <main className="mt-8 container mx-auto px-4">
+    <main className="mt-32 container mx-auto px-4 max-[525px]:mt-48">
       <div className="w-full flex items-center justify-between">
         <h2 className="font-semibold text-2xl">Vagas disponíveis</h2>
         <RoleSelect />

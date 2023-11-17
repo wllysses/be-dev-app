e60ae47812a854f05360f9bfd52bf985440fd0de
wllysses <wllyssestavares@gmail.com>
2023-11-17 15:45:39 -0300

    styles: add margin-bottom on main component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Vacancies } from "@/components/ui/vacancies";
 
 export default function Home() {
   return (
-    <main className="mt-32 container mx-auto px-4 max-[525px]:mt-48">
+    <main className="mt-32 mb-8 container mx-auto px-4 max-[525px]:mt-48">
       <div className="w-full flex items-center justify-between">
         <h2 className="font-semibold text-2xl">Vagas disponíveis</h2>
         <RoleSelect />
